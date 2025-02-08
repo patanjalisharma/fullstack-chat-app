bd2e073d78e4efcba4f88f0ed8cc8aaa73178c09
patanjalisharma <patanjali.sharma03@gmail.com>
2025-02-08 18:53:39 +0530

    Project completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -51,7 +51,7 @@ export const useAuthStore = create((set, get) => ({
 
       get().connectSocket();
     } catch (error) {
-      console.log(error)
+      toast.error(error.response.data.message);
     } finally {
       set({ isLoggingIn: false });
     }
